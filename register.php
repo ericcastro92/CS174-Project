e8e08005138c890964f6dea2a6246392aa8b80fe
Eric Castro <ericcastro92@gmail.com>
2014-11-29 21:37:49 -0800

    Form checking for registration

--- register.php
+++ register.php
@@ -2,7 +2,42 @@
 	include 'dbconnect.php';
 	if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST')
 	{
-		//Login
+		$email = $_POST['email'];
+		$pass = $_POST['password'];
+		$repass = $_POST['repassword'];
+
+		/**
+		DEBUG: REMOVE WHEN DONE
+		print $email."<br>";
+		print $pass."<br>";
+		print $email."<br>";
+		*/
+
+		$validform = true;
+
+		//Check for empty strings
+		if(empty($email) || empty($pass) || empty($repass))
+		{
+			print "Please fill out all input fields.<br>";
+			$validform = false;
+		}
+
+		//Check for 8 characters
+		if(strlen($pass) < 8 || strlen($pass) > 20){
+			print "Password must be 8 to 20 characters.<br>";
+			$validform = false;
+		}
+
+		//Check for Uppercase, Lowercase and number
+		if(!preg_match("/[A-Z]+[a-z]+/", $pass)){
+			print 'Password must have at least 1 uppercase and 1 lowercase letter<br>';
+			$validform = false;
+		}
+
+		if(!preg_match('[\d]', $pass)){
+			print 'Password must have at least 1 number<br>';
+			$validform = false;
+		}
 	}
 ?>
 <html>
