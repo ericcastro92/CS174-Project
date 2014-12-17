be5616b7067041b249da49359f576a0309c961c4
arjun239 <arjun_239@hotmail.co.uk>
2014-12-17 12:41:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 		<title>
 			CS174 - Group 1 - Home
 		</title>
+		
+		<form name="form 1" method="post" action="searchresults.php"
+                <input name="search" type="text" size="40" maxlength="50"/>
+                <input type="submit" name="submit" value="Search"/>
 		<!-- Slideshow script -->
 		<script src="js/slideshow.js"></script>
 		<!-- Index page css -->
